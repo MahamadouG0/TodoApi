18b05080f1f999f467344241e9563ef2d89354d4
MahamadouG0 <116963400+MahamadouG0@users.noreply.github.com>
2025-11-14 11:46:08 +0100

    Ajoutez des fichiers projet.

---
+++ TodoApi/Todo.cs
@@ -0,0 +1,16 @@
+﻿//namespace TodoApi
+//{
+//    public class Todo
+//    {
+//    }
+//}
+
+namespace TodoApi
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,19 @@
+//namespace TodoApi
+//{
+//    public class TodoDb
+//    {
+//    }
+//}
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
